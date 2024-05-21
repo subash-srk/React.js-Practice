9844e8e39ad944983d3553a531eeafcf926747d0
Subash K <81848034+subash-srk@users.noreply.github.com>
2024-05-21 21:11:09 +0530

    Add files via upload

---
+++ managing-a-component-tree/src/components/ToDoItem.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function ToDoItem(props) {
+  function changeStyle() {}
+  return (
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.text}</li>
+    </div>
+  );
+}
+
+export default ToDoItem;
